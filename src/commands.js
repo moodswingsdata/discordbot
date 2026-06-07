b6b14c38ce676663d96fafe22d6acd0cdd328fef
Matt Cooper <vtbassmatt@gmail.com>
2026-06-07 07:14:00 -0400

    command registration

---
+++ src/commands.js
@@ -0,0 +1,9 @@
+export const FEEL_COMMAND = {
+  name: 'feel',
+  description: 'Look up a card by name.',
+};
+
+export const SEARCH_COMMAND = {
+  name: 'search',
+  description: 'Run a search using Feelings syntax.',
+};
